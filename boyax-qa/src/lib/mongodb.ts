0a960c782bbe0d0124e069aed7cfd83a4d5b6677
Lawrence <claudiuslawrence@gmail.com>
2025-08-21 12:16:43 +0400

    testing

--- boyax-qa/src/lib/mongodb.ts
+++ boyax-qa/src/lib/mongodb.ts
@@ -19,7 +19,9 @@ if (process.env.NODE_ENV === "development") {
   clientPromise = (global as any)._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  (global as any)._mongoClientPromise = client.connect();
+  clientPromise = (global as any)._mongoClientPromise;
+//   clientPromise = client.connect();
 }
 
 export default clientPromise;
